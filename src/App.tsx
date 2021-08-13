1761e1f29e8ca09a2d6bd4017589607b5aca8299
rkh-leung <leptanian@gmail.com>
2021-08-13 16:12:21 +1200

    add: content user story 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
 import React, { useState } from 'react'
-import './App.css';
+import './App.css'
 
 function App() {
   let breakState: number = 5
+  let sessionState: number = 1500
+
   const [breakLength, setBreakLength] = useState(breakState)
-  let sessionState: number = 25
-  const [sessionLength, setSessionLength] = useState(sessionState)
+  const [second, setSecond] = useState(sessionState)
+
+  let minute: number = Math.floor(second / 60)
+
+  const sessionHandler = (counter: number) => {
+    setSecond((prevState) => prevState + counter)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        FCC 25+5 Clock
-      </header>
+    <div className='App'>
+      <header className='App-header'>FCC 25+5 Clock</header>
       <div id={'break-label'}>Break Length</div>
-      <div id={'session-label'}>Session Length</div>
-      <button id={'break-decrement'} />
-      <button id={'session-decrement'} />
-      <button id={'break-increment'} />
-      <button id={'session-increment'} />
+      <button id={'break-decrement'}>
+        <i className={'fa fa-arrow-down'} />
+      </button>
+      <button id={'break-increment'}>
+        <i className={'fa fa-arrow-up'} />
+      </button>
       <div id={'break-length'}>{breakLength}</div>
-      <div id={'session-length'}>{sessionLength}</div>
+      <div id={'session-label'}>Session Length</div>
+      <i
+        className={'fa fa-arrow-down'}
+        id={'session-decrement'}
+        onClick={() => sessionHandler(-60)}
+      />
+      <span id={'session-length'}>{minute}</span>
+      <i
+        className={'fa fa-arrow-up'}
+        id={'session-increment'}
+        onClick={() => sessionHandler(60)}
+      />
       <div id={'timer-label'}>Session</div>
+      <div id={'time-left'}>
+        {minute < 10 ? `0${minute}` : minute}:
+        {second % 60 < 10 ? `0${second % 60}` : second % 60}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
